7dca7a3cd8849c7242613342efea089d09d4f7b8
WestKostikov <maksrey@mail.ru>
2020-08-26 21:19:19 +0300

    Add comments

--- app.rb
+++ app.rb
@@ -8,12 +8,23 @@ def init_db
 	@db.results_as_hash = true 
 end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы 
 before do	
+	# инициализация БД
 	init_db
 end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
+
 configure do
+	# инициализация БД 
+
 	init_db
+
+	# создаёт таблицу, если таблица не существует
+
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts (
 	id	INTEGER,
 	created_date	DATE,
@@ -26,11 +37,18 @@ get '/' do
     erb "Hello!"			
 end
 
+# обработчик get-запроса /new
+# (браузер получает страницу с сервера)
+
 get '/new' do
   erb :new  
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+	# получаем переменную из post-запроса
   content = params[:content]
+
   erb "You typed #{content}"
 end
